1e6fa646e931329d4b09850797fd7e860d68fb81
Thyran <benedikt-wbach@t-online.de>
2025-01-10 11:51:05 +0100

    fixed smb plugin version when using custom plugin_platform

--- csi-driver-smb/build.sh
+++ csi-driver-smb/build.sh
@@ -18,7 +18,7 @@ docker plugin disable $ORG/swarm-csi-smb:v$VERSION_PLATFORM
 docker plugin rm $ORG/swarm-csi-smb:v$VERSION_PLATFORM
 docker rm -vf rootfsimage
 
-docker create --name rootfsimage registry.k8s.io/sig-storage/smbplugin:v$VERSION_PLATFORM
+docker create --name rootfsimage registry.k8s.io/sig-storage/smbplugin:v$VERSION
 mkdir -p rootfs
 docker export rootfsimage | tar -x -C rootfs
 docker rm -vf rootfsimage
